03bc35ab1bd86c37b10c1b4b17cad9a12db02136
manish_singh <manishkandari43@gmail.com>
2025-02-04 20:50:11 +0530

    04-05-2025

--- backend/db.py
+++ backend/db.py
@@ -8,7 +8,7 @@ if not MONGO_URI:
     raise ValueError("MONGODB_URI is not set in .env")
 
 client = MongoClient(MONGO_URI)
-
+#  hello word
 try:
     client.admin.command('ping')
     print("MongoDB connected successfully!")
